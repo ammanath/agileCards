36db3496e91603aa3a73560d907eff3ad80c535a
bit00 <akbrijesh@gmail.com>
2020-04-15 23:21:43 +0530

    upates

--- lib/agileCardsHome.dart
+++ lib/agileCardsHome.dart
@@ -1,38 +1,43 @@
 import 'package:agile_cards/DataListWidget.dart';
 import 'package:agile_cards/dataText.dart';
+import 'package:agile_cards/widgetsPage.dart';
 import 'package:flutter/material.dart';
 
 class AgileCardsHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<DataText> dataText1 = getAgileValues();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Agile Cards'),
-      ),
-      body: SingleChildScrollView(child: DataListWidget(        dataText: dataText1,      )),
-      backgroundColor: Colors.red,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1, // this will be set when a new tab is tapped
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.navigate_before),
-            title: new Text('Previous'),
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text('Agile Cards'), actions: <Widget>[
+          // action button
+          IconButton(
+            icon: Icon(choices[0].icon),
+            tooltip: choices[0].toolTip,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>WidgetsPage()));
+            },
           ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            title: new Text('Home'),
+          // action button
+          IconButton(
+            icon: Icon(choices[1].icon),
+            onPressed: () {
+              //_select(choices[1]);
+            },
           ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.navigate_next), title: Text('Next'))
-        ],
-        backgroundColor: Colors.red[200],
+        ]),
+        body: SingleChildScrollView(
+            child: DataListWidget(
+          dataText: dataText1,
+        )),
+        backgroundColor: Colors.red,
       ),
     );
   }
 
   List<DataText> getAgileValues() {
-    const primaryWorkingText = 'Working Software ';
+    const primaryWorkingText = 'Working Software';
     const secondaryWorkingText = "over comprehensive documentation.";
     const primaryRespondingText = 'Responding to change ';
     const secondaryRespondingText = 'over following a plan';
@@ -66,18 +71,12 @@ class AgileCardsHome extends StatelessWidget {
         'Providing ',
         'motivated individuals with the envt. and support they need and trusting them to get the job done'
       ]),
-      DataText(iconPrinciple, [
-        'Creating processes ',
-        'that promote sustainable efforts'
-      ]),
-      DataText(iconPrinciple, [
-        'Maintaining ',
-        'a constant pace for completed work'
-      ]),
-      DataText(iconPrinciple, [
-        'Welcoming ',
-        'changing requirements, even late in a project'
-      ]),
+      DataText(iconPrinciple,
+          ['Creating processes ', 'that promote sustainable efforts']),
+      DataText(iconPrinciple,
+          ['Maintaining ', 'a constant pace for completed work']),
+      DataText(iconPrinciple,
+          ['Welcoming ', 'changing requirements, even late in a project']),
       DataText(iconPrinciple, [
         'Assembling ',
         'the project team and business owners on a daily basis throughout the project'
@@ -86,20 +85,26 @@ class AgileCardsHome extends StatelessWidget {
         'Having ',
         'the team reflect at regular intervals on how to become more effective, then tuning and adjusting behaviour accordingly'
       ]),
-      DataText(iconPrinciple, [
-        'Measuring progress ',
-        'by the amount of completed work'
-      ]),
-      DataText(iconPrinciple, [
-        'Continually ',
-        'seeking excellence'
-      ]),
-      DataText(iconPrinciple, [
-        'Harnessing change ',
-        'for a competitive advantage'
-      ]),
-
+      DataText(iconPrinciple,
+          ['Measuring progress ', 'by the amount of completed work']),
+      DataText(iconPrinciple, ['Continually ', 'seeking excellence']),
+      DataText(
+          iconPrinciple, ['Harnessing change ', 'for a competitive advantage']),
     ];
     return dataText1;
   }
 }
+
+class Choice {
+  const Choice({this.title, this.icon, this.toolTip=''});
+
+  final String title;
+  final IconData icon;
+
+  final toolTip ;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Home', icon: Icons.home, toolTip: 'Home'),
+  const Choice(title: 'Rate', icon: Icons.rate_review),
+];
