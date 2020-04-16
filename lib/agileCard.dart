27489f0e5e1107f99d491c47a0e7cb272d2e57f5
bit00 <akbrijesh@gmail.com>
2020-04-16 20:37:36 +0530

    Added animation to the dialog box text

--- lib/agileCard.dart
+++ lib/agileCard.dart
@@ -1,6 +1,6 @@
 import 'package:agile_cards/dataText.dart';
 import 'package:flutter/material.dart';
-
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class AgileCard extends StatelessWidget {
   const AgileCard({
@@ -13,51 +13,76 @@ class AgileCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var secondaryStyle = TextStyle(
-                          fontSize: 14,
-                          color: Colors.black38,
-                        );
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            child: Container(
-              child: ListTile(
-                leading: Icon(dataText.icon),
-            title: Text.rich(
-              TextSpan(
+      fontSize: 14,
+      color: Colors.black38,
+    );
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Container(
+          child: ListTile(
+              leading: Icon(dataText.icon),
+              title: Text.rich(
+                TextSpan(
                   text: dataText.primaryText,
                   style: TextStyle(fontSize: 25, color: Colors.black),
                   children: getSecondaryText(secondaryStyle, dataText),
-              
+                ),
               ),
+              onTap: () => {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return PopUpTextDisplay(dataText: dataText);
+                        }),
+                  }),
+          color: Colors.orange,
         ),
-        onTap: () =>{
-          showDialog(context: context,
-          builder:(context){
-            return AlertDialog(
-              //title:Text("Value"),
-              content:Text("${dataText.primaryText} ${dataText.secondaryText}"),
-            );
-          }
-          ),
-
-        }
       ),
-      color: Colors.orange,
-              ),
-            ),
     );
   }
 
-  List<InlineSpan> getSecondaryText(TextStyle secondaryStyle,DataText dataText1) {
-   return dataText1.textList.map((elt)=>(TextSpan(
-                      text: elt,
-                      style: secondaryStyle))).toList()..removeAt(0);
-
+  List<InlineSpan> getSecondaryText(
+      TextStyle secondaryStyle, DataText dataText1) {
+    return dataText1.textList
+        .map((elt) => (TextSpan(text: elt, style: secondaryStyle)))
+        .toList()
+          ..removeAt(0);
 
     // return <InlineSpan>[
     //               TextSpan(
     //                   text: dataText1.secondaryText,
     //                   style: secondaryStyle)
     //         ];
+  }
+}
+
+class PopUpTextDisplay extends StatelessWidget {
+  const PopUpTextDisplay({
+    Key key,
+    @required this.dataText,
+  }) : super(key: key);
+
+  final DataText dataText;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        //title:Text("Value"),
+        backgroundColor: Colors.orange[50],
+        content: TypewriterAnimatedTextKit(
+          text: ["${dataText.primaryText} ${dataText.secondaryText}"],
+          totalRepeatCount: 4,
+          pause: Duration(milliseconds: 1000),
+          textStyle: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.normal, //fontStyle: FontStyle.italic,
+            fontFamily: "Segoe UI",
+            backgroundColor: Colors.orange[50],
+            color: Colors.brown,
+          ),
+          displayFullTextOnTap: true,
+          stopPauseOnTap: true,
+        ));
   }
 }
