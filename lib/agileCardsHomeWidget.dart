d47f04ed9519057bff8d17b6ee9853985a469a75
bit00 <akbrijesh@gmail.com>
2020-04-24 20:55:26 +0530

    added icon for whatsapp and rate

--- lib/agileCardsHomeWidget.dart
+++ lib/agileCardsHomeWidget.dart
@@ -17,7 +17,22 @@ class AgileCardsHomeWidget extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: Text('Agile Cards'), actions: <Widget>[
-          
+          IconButton(
+            icon: Icon(Icons.whatshot),
+            color: Colors.amber,
+            onPressed: () {
+            },
+          ),
+          // action button
+          IconButton(
+            icon: Icon(Icons.star),
+            color: Colors.amber,
+            onPressed: () {
+              dlw.itemScrollController.jumpTo(
+                index: 1,
+              );
+            },
+          ),
         ]),
         body: SingleChildScrollView(
           child: dlw,
