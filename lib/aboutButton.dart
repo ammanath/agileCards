ae6dad398598469f7e3caf33b0a7401ea02bfb51
bit00 <akbrijesh@gmail.com>
2020-05-09 20:13:54 +0530

    about app

--- lib/aboutButton.dart
+++ lib/aboutButton.dart
@@ -24,13 +24,13 @@ class AboutButton extends StatelessWidget {
           return AlertDialog(
             title: Center(child: Text("About Agile Cards")),
             content: Text(
-                "A quick primer about Agile Values and Principles \nThis is the first version of the App. Future versions will cover other specific Agile and Lean s/w development frameworks"),
+                "A quick primer about Agile Software Development Methodologies and some key principles for cutting top quality code. \n If you have any suggestions about the app, then I will be much obliged if you drop me a note : swapp@fintechthoughtleaders.com \n \n Thanks for trying my app! \n- Brijesh"),
             elevation: 24.0,
-            backgroundColor: Colors.blue,
+            backgroundColor: Colors.blue[100],
             actions: <Widget>[
               FlatButton(
                 child: Text('Ok'),
-                textColor: Colors.white,
+                textColor: Colors.blue,
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
